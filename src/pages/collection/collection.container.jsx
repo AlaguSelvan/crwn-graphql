92d4454ff54824b3d268876cb2eda17696c24c8f
AlaguSelvan <alaguselvan97@gmail.com>
2020-01-10 20:32:29 +0530

    first commit

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+import CollectionPage from './collection.component'
+import Spinner from '../../components/spinner/spinner.component'
+
+const GET_COLLECTION_BY_TITLE = gql`
+  query getCollectionsByTitle($title: String!) {
+    getCollectionsByTitle(title: $title)
+  }
+`
+
+const CollectionPageContainer = ({ match }) => (
+  <Query query={GET_COLLECTION_BY_TITLE} variables={{ title: match.params.collectionId}}>
+    {
+      ({loading, data: { getCollectionsByTitle }}) => {
+        if(loading) return <Spinner />
+        return <CollectionPage collection={getCollectionsByTitle} />
+      }
+    }
+  </Query>
+)
+
+export default CollectionPageContainer
